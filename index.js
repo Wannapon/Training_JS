fda136d848488469b1e1ffb5dd21ff66d40ddf3a
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2020-07-22 17:01:02 +0700

    day 1.2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello world")
+// function
+var b = 0
+function logtest() {
+    let a = 'Hello'
+
+    return a
+}
+var logtest2 = () => b 
+logtest()
+logtest2()
+console.log(logtest(),logtest2())
+
+
+// switch case
+const switchmatch = 1
+switch (switchmatch) {
+  case 1:
+    console.log('match 1')
+    break
+  case 2:
+    console.log('match 2')
+    break
+  default:
+    console.log('no match -> default')
+}
+
+
+// .map
+const arr = [0,1,2]
+const Newarr = arr2.map((arr2) => {
+  return arr2
+})
+
+
+// calculate
+let number = 0
+number = number + 5
+console.log(number)
